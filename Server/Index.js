86a620c7e4996241ac473a6b6904be63b0cd079b
Sahil Patel <112198439+sahilpate-221s@users.noreply.github.com>
2025-07-31 17:15:51 +0530

    fixed bugs came while production

--- Server/Index.js
+++ Server/Index.js
@@ -20,13 +20,13 @@ dotenv.config();
 
 database.connect();
  
-const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:3000";
+// const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:3000";
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin: FRONTEND_URL,
+		origin: 'https://studynotionapp21.netlify.app',
 		credentials: true,
 	})
 );
