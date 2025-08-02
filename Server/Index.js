0c505d13bdfb1dbf949f9f09417d80c26260f78d
Sahil Patel <112198439+sahilpate-221s@users.noreply.github.com>
2025-08-03 00:02:43 +0530

    fixed axios error while deployemnt

--- Server/Index.js
+++ Server/Index.js
@@ -20,21 +20,11 @@ dotenv.config();
 
 database.connect();
 app.use(cors({
-//   origin: "https://studynotionapp21.netlify.app", // allow only Netlify deployed frontend
-	origin: "http://localhost:3000",
+  origin: "https://studynotionapp21.netlify.app", // allow only Netlify deployed frontend
+	// origin: "http://localhost:3000",
   credentials: true, // allow cookies/auth headers if used
 }));
 
-// Middleware to set CORS headers on all responses including errors
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "https://studynotionapp21.netlify.app");
-//   res.header("Access-Control-Allow-Credentials", "true");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-//   next();
-// });
- 
-// const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:3000";
 
 app.use(express.json());
 app.use(cookieParser());
